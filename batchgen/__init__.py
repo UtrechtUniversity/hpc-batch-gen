b77058268222ff89b4da71d415aa9a8c625fb7ed
Raoul Schram <r.d.schram@uu.nl>
2019-03-07 13:41:37 +0100

    Version 0.0.4: Use parallel for SLURM.
    New options in the configuration file:
    
    - num_cores_simul: number of simultaneously running tasks
    - num_tasks_per_node: number of tasks assigned per node

--- batchgen/__init__.py
+++ batchgen/__init__.py
@@ -1,3 +1,3 @@
 from batchgen.base import generate_batch_scripts
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
--- batchgen/backend/slurm_lisa.py
+++ batchgen/backend/slurm_lisa.py
@@ -11,14 +11,15 @@ from batchgen.backend.hpc import HPC, double_substitute
 from batchgen.util import mult_time
 
 
-def _get_body(script_lines):
+def _get_body(script_lines, num_cores_simul):
     """Function to create the body of the script files, staging their start.
 
     Arguments
     ---------
     script_lines: str
         List of strings where each element is one command to be submitted.
-
+    sum_cores_simul: int
+        Number of cores used simultaneously.
     Returns
     -------
     str:
@@ -26,13 +27,12 @@ def _get_body(script_lines):
     """
 
     # Stage the commands every 1 second.
-    body = ""
+    body = "parallel -j {num_cores_simul} << EOF_PARALLEL\n"
+    body = body.format(num_cores_simul=num_cores_simul)
     for line in script_lines:
-        new_line = line.rstrip() + " &> /dev/null &\n"
+        new_line = line.rstrip() + " &> /dev/null\n"
         body = body+new_line
-        body = body+"sleep 1\n"
-    body += "wait\n"
-    body += "wait"
+    body += "EOF_PARALLEL\n"
     return body
 
 
@@ -67,10 +67,17 @@ date
             num_cores = int(param["num_cores"])
         else:
             num_cores = 16
+        if "num_cores_simul" not in param:
+            param["num_cores_simul"] = num_cores
+        if "num_tasks_per_node" not in param:
+            param["num_tasks_per_node"] = param["num_cores_simul"]
+        param["num_cores_simul"] = int(param["num_cores_simul"])
+        param["num_tasks_per_node"] = int(param["num_tasks_per_node"])
 
-        num_jobs = len(script_lines)
-        max_num_cores = max(num_cores, num_jobs)
-        num_nodes = (num_jobs-1) // num_cores + 1
+        tasks_per_node = param["num_tasks_per_node"]
+        num_tasks = len(script_lines)
+        max_num_cores = min(num_cores, num_tasks)
+        num_nodes = (num_tasks-1) // tasks_per_node + 1
         cost_factor = 16*num_nodes
 
         param["script_lines"] = script_lines
@@ -78,10 +85,11 @@ date
         param["num_cores"] = num_cores
         param["max_num_cores"] = max_num_cores
         param["num_nodes"] = num_nodes
-        param["num_jobs"] = num_jobs
+        param["num_tasks"] = num_tasks
 
         param["max_bill_time"] = mult_time(param["clock_wall_time"],
                                            cost_factor)
+
         return param
 
     def _write_batch_files(self):
@@ -89,15 +97,18 @@ date
         script_lines = par["script_lines"]
         num_cores = par["num_cores"]
         output_dir = par["output_dir"]
+        num_tasks = par["num_tasks"]
+        tpn = par["num_tasks_per_node"]
+        ncs = par["num_cores_simul"]
         # Split the commands in batches.
-        for batch_id, i in enumerate(range(0, len(script_lines), num_cores)):
+        for batch_id, i in enumerate(range(0, num_tasks, tpn)):
             # Output file
             output_file = os.path.join(output_dir,
                                        "batch" + str(batch_id) + ".sh")
-            par["main_body"] = _get_body(script_lines[i:i + num_cores])
+            par["main_body"] = _get_body(script_lines[i:i+tpn], ncs)
             par["batch_id"] = batch_id
-            if len(script_lines[i:i+num_cores]) < num_cores:
-                par["num_cores"] = len(script_lines[i:i + num_cores])
+            if len(script_lines[i:i+tpn]) < num_cores:
+                par["num_cores"] = len(script_lines[i:i+tpn])
 
             # Allow for one more substitution to facilitate user substitution.
             batch_script = double_substitute(self._batch_template, par)
@@ -116,12 +127,14 @@ date
 ******************************************************
 **                 Running parameters               **
 ******************************************************
-** Job name        : {job_name: <31}**
-** Number of jobs  : {num_jobs: <31}**
-** Cores per node  : {max_num_cores: <31}**
-** Maximum run time: {clock_wall_time: <31}**
-** Number of nodes : {num_nodes: <31}**
-** Max billing time: {max_bill_time: <31}**
+** Job name          : {job_name: <29}**
+** Number of tasks   : {num_tasks: <29}**
+** Tasks per node    : {num_tasks_per_node: <29}**
+** Cores per node    : {max_num_cores: <29}**
+** Simultaneous tasks: {num_cores_simul: <29}**
+** Maximum run time  : {clock_wall_time: <29}**
+** Number of nodes   : {num_nodes: <29}**
+** Max billing time  : {max_bill_time: <29}**
 ******************************************************
 ** Execute the following on the command line (bash) **
 ******************************************************
